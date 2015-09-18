927585e1b5e186e8db8400b2293fdffdb76e6168
Thomas Pun <thomas@pun.io>
2015-09-18 11:48:29 +0800

    added referred count

--- server/server.es6.js
+++ server/server.es6.js
@@ -9,10 +9,11 @@ Meteor.methods({
       website : website,
       referrerId : referrerId
     }, function() {
+      let count = Promos.find({referrerId:referrerId}).count();
       Email.send({
         from : 'promo@askdouble.com',
         to : 'thomas@double.co',
-        subject: `New Referral from ${referrer.profile.firstname}`,
+        subject: `New Referral from ${referrer.profile.firstname} (${count} th)`,
         text: `${name} of ${website} has signed up with ${email}, YEAH!!!!`
       });
     });
