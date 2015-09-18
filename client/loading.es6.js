22bff1dbe7ef5f0b11ddd464be5a3e8f0ab68402
Thomas Pun <thomas@pun.io>
2015-09-18 17:18:33 +0800

    added loading screen (cannot get the spinner to work thou.)

---
+++ client/loading.es6.js
@@ -0,0 +1,21 @@
+Template.loading.rendered = function () {
+  if (! Session.get('loadingSplash') ) {
+    this.loading = window.pleaseWait({
+      logo: '/double-logo.png',
+      backgroundColor: '#FAFAFA',
+      loadingHtml: message
+    });
+    Session.set('loadingSplash', true); // just show loading splash once
+  }
+};
+
+Template.loading.destroyed = function () {
+  if ( this.loading ) {
+    this.loading.finish();
+  }
+};
+
+var message = '<p class="loading-message">Loading...</p>';
+// var spinner = '<div class="sk-spinner sk-bouncedelay"></div>';
+// var spinner = '<div class="sk-spinner sk-spinner-rotating-plane"></div>';
+// var spinner = '<div class="sk-spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
