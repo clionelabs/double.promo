4fed8fed37985acae577a673eaacf29f8021833d
Thomas Pun <thomas@pun.io>
2015-10-13 14:53:00 +0800

    display discount code

--- client/signup.es6.js
+++ client/signup.es6.js
@@ -5,12 +5,35 @@ Template.signup.onRendered(function() {
 Template.signup.events({
   "submit #signup" : function(e, tmpl) {
     e.preventDefault();
-    let name = e.target['full-name'].value;
-    let email = e.target.email.value;
-    let slack = e.target['slack'].value;
-    // TODO: add promo code
-    Meteor.call('signup', name, email, slack, function() {
+    const name = e.target['full-name'].value;
+    const email = e.target.email.value;
+    const slack = e.target['slack'].value;
+    const code = this.data;
+    Meteor.call('signup', name, email, slack, code, function() {
       Router.go('/success');
     });
   }
-})
+});
+
+Template.signup.helpers({
+  discountCode() {
+    const promoCode = Template.instance().data.promoCode;
+    return promoCode.code;
+  },
+  minuteRateDiscountPercent() {
+    const promoCode = Template.instance().data.promoCode;
+    return promoCode.minuteRateDiscountPercent;
+  },
+  minuteCredit() {
+    const promoCode = Template.instance().data.promoCode;
+    return promoCode.additionalMinuteCredit;
+  },
+  validTillString() {
+    const promoCode = Template.instance().data.promoCode;
+    return moment(promoCode.validTill).format('MMM Do, YYYY');
+  },
+  effectiveRate() {
+    const promoCode = Template.instance().data.promoCode;
+    return 6.0 * (100-promoCode.minuteRateDiscountPercent) / 100.0;
+  }
+});
--- server/publishes.es6.js
+++ server/publishes.es6.js
@@ -4,3 +4,7 @@ Meteor.publish('referrals', function(code) {
   if (crusor.count()==0) return [];
   return [ crusor ];
 });
+
+Meteor.publish('promoCodes', function(code) {
+  return PromoCodes.find({code: code});
+})
